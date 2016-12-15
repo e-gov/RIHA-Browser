d6b4372d7f4f1529b33e9774f170d3a810a79efb
Kirill Klenski, Kristo Kuiv <kirill@codeborne.com, kristo@codeborne.com>
2016-12-15 15:50:41 +0200

    run javascript tests when building with maven

--- karma-conf.js
+++ karma-conf.js
@@ -12,9 +12,7 @@ module.exports = function(karma) {
 
     // list of files / patterns to load in the browser
     files: [
-      'src/main/resources/static/js/vendor/tether.min.js',
       'src/main/resources/static/js/vendor/jquery-3.1.1.min.js',
-      'src/main/resources/static/js/vendor/bootstrap.min.js',
       'src/main/resources/static/js/vendor/jquery.dataTables.min.js',
       'src/main/resources/static/js/**/*.js',
       'src/test/js/**/*.js',
