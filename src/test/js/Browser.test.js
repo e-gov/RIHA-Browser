93f7a27a7fb45cc5b3ccf54358f301f4588895b5
Kirill Klenski, Kristo Kuiv <kirill@codeborne.com, kristo@codeborne.com>
2016-12-15 12:06:30 +0200

    [#135840369] use new RIHA-API version

--- src/test/js/Browser.test.js
+++ src/test/js/Browser.test.js
@@ -2,36 +2,40 @@ describe('Browser', function() {
 
   var data = [
     {
-      "owner": "70000562",
-      "meta": {
-        "URI": "/70000562/Eesti kirikuregister",
-        "timestamp": "2015-09-05T00:36:26.255215"
-      },
-      "documentation": "https://riha.eesti.ee/riha/main/inf/eesti_kirikute_koguduste_ja_koguduste_liitude_register",
       "name": "Eesti kirikute, koguduste ja koguduste liitude register",
       "shortname": "Eesti kirikuregister",
-      "status": {
-        "staatus": "Lõpetatud",
-        "timestamp": "2015-08-05T08:29:58.328468"
+      "owner": {
+        "code": "70000562",
+        "name": "Siseministeerium"
       },
-      "approval": {
-        "timestamp": "2016-12-07T12:16:15.847",
-        "status": "MITTE KOOSKÕLASTATUD"
-      }
+      "documentation": "eesti_kirikute_koguduste_ja_koguduste_liitude_register",
+      "meta": {
+        "system_status": {
+          "status": "INFOSYS_STAATUS_LOPETATUD",
+          "timestamp": "2015-09-05T00:36:26.255215"
+        },
+        "approval_status": {
+          "status": "MITTE KOOSKÕLASTATUD",
+          "timestamp": "2016-09-05T00:36:26.255215"
+        }
+      },
+      "uri": "http://172.31.34.244:8090/Eesti%20kirikuregister"
     },
     {
-      "owner": "70000740",
-      "meta": {
-        "URI": "/70000740/Õppurite register",
-        "timestamp": "2013-11-14T13:43:55.546948"
-      },
-      "documentation": "https://riha.eesti.ee/riha/main/inf/opilaste_ja_uliopilaste_register",
       "name": "Õpilaste ja üliõpilaste register",
       "shortname": "Õppurite register",
-      "status": {
-        "staatus": "Lõpetatud",
-        "timestamp": "2013-11-08T15:46:15.121725"
-      }
+      "owner": {
+        "code": "70000740",
+        "name": "Haridus- ja Teadusministeerium"
+      },
+      "documentation": "opilaste_ja_uliopilaste_register",
+      "meta": {
+        "system_status": {
+          "status": "INFOSYS_STAATUS_LOPETATUD",
+          "timestamp": "2013-11-14T13:43:55.546948"
+        }
+      },
+      "uri": "http://172.31.34.244:8090/%C3%95ppurite%20register"
     }
   ];
 
@@ -45,10 +49,10 @@ describe('Browser', function() {
     expect(rows.length).toBe(2);
     expect($(rows[0]).find('.name').text()).toBe('Eesti kirikute, koguduste ja koguduste liitude register');
     expect($(rows[0]).find('.owner').text()).toBe('70000562');
-    expect($(rows[0]).find('.last-modified').text()).toBe('2015-08-05T08:29:58.328468');
-    expect($(rows[0]).find('.status').text()).toBe('Lõpetatud');
+    expect($(rows[0]).find('.last-modified').text()).toBe('2015-09-05T00:36:26.255215');
+    expect($(rows[0]).find('.status').text()).toBe('INFOSYS_STAATUS_LOPETATUD');
     expect($(rows[0]).find('.approval-status').text()).toBe('MITTE KOOSKÕLASTATUD');
-    expect($(rows[0]).find('.approved').text()).toBe('2016-12-07T12:16:15.847');
+    expect($(rows[0]).find('.approved').text()).toBe('2016-09-05T00:36:26.255215');
   });
 });
 
