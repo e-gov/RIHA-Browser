9c477dc4826071b0fb69e932050920f1140702c0
mark.kimask <mark.kimask@wisercat.eu>
2017-06-07 16:20:00 +0300

    modify components to fit stilistically

--- src/frontend/src/app/browser-list/browser-list.component.ts
+++ src/frontend/src/app/browser-list/browser-list.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BrowserListComponent implements OnInit {
 
-  constructor() { }
+  systems: any[];
+
+  constructor() {
+    this.systems = [];
+  }
 
   ngOnInit() {
   }
--- src/frontend/src/app/producer-list/producer-list.component.ts
+++ src/frontend/src/app/producer-list/producer-list.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProducerListComponent implements OnInit {
 
-  constructor() { }
+  systems: any[];
+
+  constructor() {
+    this.systems = [];
+  }
 
   ngOnInit() {
   }
