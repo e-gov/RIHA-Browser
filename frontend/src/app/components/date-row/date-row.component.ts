59a8107da310de233d52c66e25a1d02df8669f08
mark.kimask <mark.kimask@wisercat.eu>
2017-06-22 14:34:50 +0300

    RIHAKB-153 RIHAKB-137 add component to show update date

---
+++ frontend/src/app/components/date-row/date-row.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-date-row',
+  templateUrl: './date-row.component.html',
+  styleUrls: ['./date-row.component.scss']
+})
+export class DateRowComponent implements OnInit {
+
+  @Input() text: string;
+  @Input() date: string;
+
+  formattedDate: string;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
