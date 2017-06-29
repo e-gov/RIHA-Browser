3ed5d141c11b236bd02d869c6848f774d56abb33
Valentin Suhnjov <valentin.suhnjov@wisercat.eu>
2017-06-29 11:17:18 +0300

    RIHAKB-173. Implement environment service that provides remote API URLs

--- backend/src/main/java/ee/ria/riha/BrowserApplication.java
+++ backend/src/main/java/ee/ria/riha/BrowserApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BrowserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BrowserApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BrowserApplication.class, args);
+    }
 }
--- backend/src/main/java/ee/ria/riha/web/ApplicationController.java
+++ backend/src/main/java/ee/ria/riha/web/ApplicationController.java
@@ -1,5 +1,8 @@
 package ee.ria.riha.web;
 
+import ee.ria.riha.service.ApplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +15,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 public class ApplicationController {
 
+    @Autowired
+    private ApplicationService applicationService;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Model model) {
         return "index";
+    }
+
+    @RequestMapping(value = "/env")
+    public ResponseEntity environment() {
+        return ResponseEntity.ok(applicationService.getEnvironment());
     }
 
 }
