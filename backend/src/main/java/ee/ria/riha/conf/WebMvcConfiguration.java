f59a5b6f66358735799afad8c68bfec51ab30d84
Valentin Suhnjov <valentin.suhnjov@wisercat.eu>
2017-07-28 18:01:59 +0300

    RIHAKB-227. Handle and redirect to index.html on error 404

--- backend/src/main/java/ee/ria/riha/conf/WebMvcConfiguration.java
+++ backend/src/main/java/ee/ria/riha/conf/WebMvcConfiguration.java
@@ -2,12 +2,17 @@ package ee.ria.riha.conf;
 
 import ee.ria.riha.storage.util.FilterableArgumentResolver;
 import ee.ria.riha.storage.util.PageableArgumentResolver;
+import org.springframework.boot.autoconfigure.web.ErrorViewResolver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
@@ -25,6 +30,13 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
     public void addFormatters(FormatterRegistry registry) {
         super.addFormatters(registry);
         registry.addConverter(new UUIDConverter());
+    }
+
+    @Bean
+    public ErrorViewResolver supportPathBasedLocationStrategyWithoutHashes() {
+        return (request, status, model) -> status == HttpStatus.NOT_FOUND
+                ? new ModelAndView("index.html", Collections.emptyMap(), HttpStatus.OK)
+                : null;
     }
 
     static class UUIDConverter implements Converter<String, UUID> {
