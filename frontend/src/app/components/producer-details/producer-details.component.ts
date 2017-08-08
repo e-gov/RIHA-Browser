3d2c7849d243384ecc37e351a92ff6c69598cdb8
mark.kimask <mark.kimask@wisercat.eu>
2017-08-08 17:06:30 +0300

    RIHAKB-191 jump to section from url hash

--- frontend/src/app/components/producer-details/producer-details.component.ts
+++ frontend/src/app/components/producer-details/producer-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SystemsService } from '../../services/systems.service';
 import { ActivatedRoute } from '@angular/router';
 import { System } from '../../models/system';
+import { WindowRefService } from '../../services/window-ref.service';
 
 @Component({
   selector: 'app-producer-details',
@@ -12,15 +13,33 @@ export class ProducerDetailsComponent implements OnInit {
   private system: System;
   private loaded: boolean = false;
 
+  adjustSection(attempt){
+    if (attempt < 5){
+      let hash = this.winRef.nativeWindow.location.hash;
+      if (hash){
+        let elId = decodeURI(hash.replace('#',''));
+        let el = this.winRef.nativeWindow.document.getElementById(elId);
+        if (el){
+          this.winRef.nativeWindow.scrollTo(el.offsetLeft,el.offsetTop);
+        } else {
+          attempt++;
+          setTimeout(()=>{this.adjustSection(attempt)}, 1000);
+        }
+      }
+    }
+  }
+
   getSystem(id){
     this.systemsService.getSystem(id).then(response => {
       this.system = response.json();
       this.loaded = true;
+      this.adjustSection(0);
     })
   }
 
   constructor(private systemsService: SystemsService,
-              private route: ActivatedRoute) {
+              private route: ActivatedRoute,
+              private winRef: WindowRefService) {
     this.system = new System();
   }
 
