6584aee5860523a0f63d49241f0d9842b4f5444e
Valentin Suhnjov <valentin.suhnjov@wisercat.eu>
2017-09-21 09:31:44 +0300

    RIHAKB-281. Remove approver URL from configuration

--- backend/src/main/java/ee/ria/riha/conf/ApplicationProperties.java
+++ backend/src/main/java/ee/ria/riha/conf/ApplicationProperties.java
@@ -16,7 +16,6 @@ public class ApplicationProperties {
 
     public static final String API_V1_PREFIX = "/api/v1";
 
-    private final RemoteApi remoteApi = new RemoteApi();
     private final StorageClientProperties storageClient = new StorageClientProperties();
     private final AuthenticationProperties authentication = new AuthenticationProperties();
     private final ValidationProperties validation = new ValidationProperties();
@@ -26,12 +25,6 @@ public class ApplicationProperties {
     public static class StorageClientProperties {
         @NotEmpty
         private String baseUrl;
-    }
-
-    @Getter
-    @Setter
-    public static class RemoteApi {
-        private String approverUrl;
     }
 
     @Getter
