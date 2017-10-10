9e16cfd1242e4107d7dff20de6c3822caebcd935
Valentin Suhnjov <valentin.suhnjov@wisercat.eu>
2017-10-10 21:30:03 +0300

    RIHAKB-331. Export RIHA-Storage configuration to stand alone class

--- backend/src/main/java/ee/ria/riha/conf/ApplicationConfiguration.java
+++ backend/src/main/java/ee/ria/riha/conf/ApplicationConfiguration.java
@@ -1,13 +1,9 @@
 package ee.ria.riha.conf;
 
 import com.github.fge.jackson.JsonLoader;
-import ee.ria.riha.domain.InfoSystemRepository;
-import ee.ria.riha.domain.RihaStorageInfoSystemRepository;
 import ee.ria.riha.service.JsonValidationService;
-import ee.ria.riha.storage.client.StorageClient;
-import ee.ria.riha.storage.domain.CommentRepository;
-import ee.ria.riha.storage.domain.MainResourceRepository;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -22,25 +18,10 @@ import java.io.IOException;
 public class ApplicationConfiguration {
 
     @Bean
-    public MainResourceRepository mainResourceRepository(ApplicationProperties applicationProperties) {
-        return new MainResourceRepository(getStorageClient(applicationProperties));
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
     }
 
-    private StorageClient getStorageClient(ApplicationProperties applicationProperties) {
-        RestTemplate restTemplate = new RestTemplate();
-        return new StorageClient(restTemplate, applicationProperties.getStorageClient().getBaseUrl());
-    }
-
-    @Bean
-    public InfoSystemRepository infoSystemRepository(MainResourceRepository mainResourceRepository) {
-        return new RihaStorageInfoSystemRepository(mainResourceRepository);
-    }
-
-    @Bean
-    public CommentRepository commentRepository(ApplicationProperties applicationProperties) {
-        return new CommentRepository(getStorageClient(applicationProperties));
-    }
-    
     @Bean
     public JsonValidationService jsonValidationService(ApplicationProperties applicationProperties) throws IOException {
         return new JsonValidationService(
