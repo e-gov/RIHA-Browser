27ced93b66e5650b5ac88fd16225d24bf10b4d00
Mark Kimask <mark.kimask@wisercat.eu>
2017-10-27 16:55:59 +0300

    RIHAKB-241 support hotjar conf

--- frontend/src/app/services/environment.service.ts
+++ frontend/src/app/services/environment.service.ts
@@ -61,10 +61,10 @@ export class EnvironmentService {
       ga('send', 'pageview');
     }
 
-    if(true){
+    if (environment.hjid && environment.hjsv){
       (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:670915,hjsv:6};
+        h._hjSettings={hjid:environment.hjid,hjsv:environment.hjsv};
         a=o.getElementsByTagName('head')[0];
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
