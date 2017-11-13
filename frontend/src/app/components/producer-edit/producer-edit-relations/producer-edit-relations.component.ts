a4898daffe9d1ad4c018d031bc074854693a610a
Mark Kimask <mark.kimask@wisercat.eu>
2017-11-13 20:32:23 +0200

    RIHAKB-82 enable autocomplete

--- frontend/src/app/components/producer-edit/producer-edit-relations/producer-edit-relations.component.ts
+++ frontend/src/app/components/producer-edit/producer-edit-relations/producer-edit-relations.component.ts
@@ -4,7 +4,23 @@ import { G } from '../../../globals/globals';
 import { System } from '../../../models/system';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService } from 'ngx-toastr';
+import { GeneralHelperService } from '../../../services/general-helper.service';
 
+
+//
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+
+const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado',
+  'Connecticut', 'Delaware', 'District Of Columbia', 'Federated States Of Micronesia', 'Florida', 'Georgia',
+  'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
+  'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
+  'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Islands', 'Virginia',
+  'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
 
 @Component({
   selector: 'app-producer-edit-relations',
@@ -18,13 +34,31 @@ export class ProducerEditRelationsComponent implements OnInit {
 
   globals: any = G;
   relation: {
-    infoSystemShortName: string,
+    infoSystem: any,
     type: string
   };
 
+
+  dropDownFormatter = (v)=> {
+    return `${v.details.short_name} - ${this.generalHelper.truncateString(v.details.name, 90)}`;
+  };
+
+  inputFormatter = (v)=> v.details.short_name;
+
+
+  search = (text$: Observable<string>) =>
+    text$
+      .debounceTime(200)
+      .distinctUntilChanged()
+      .switchMap(term => term.length < 2 ? []
+        : this.systemsService.getSystemsForAutocomplete(term, this.system.details.short_name));
+
+
   addRelation(addForm){
     if (addForm.valid){
-      this.systemsService.addSystemRelation(this.system.details.short_name, this.relation).then(res => {
+      let infoSystemShortName = typeof this.relation.infoSystem === 'string' ? this.relation.infoSystem : this.relation.infoSystem.details.short_name;
+      this.systemsService.addSystemRelation(this.system.details.short_name, {infoSystemShortName: infoSystemShortName,
+                                                                                     type: this.relation.type}).then(res => {
         this.refreshRelations();
         addForm.reset();
         addForm.controls.type.setValue(this.globals.relation_type.SUB_SYSTEM);
@@ -55,12 +89,13 @@ export class ProducerEditRelationsComponent implements OnInit {
   };
 
   constructor(private systemsService: SystemsService,
+              private generalHelper: GeneralHelperService,
               private toastrService: ToastrService,
               private activeModal: NgbActiveModal) { }
 
   ngOnInit() {
     this.relation = {
-      infoSystemShortName: null,
+      infoSystem: null,
       type: this.globals.relation_type.SUB_SYSTEM
     };
   }
