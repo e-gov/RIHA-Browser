ea80b12113b0449169138606160ae92bee1898da
Valentin Suhnjov <valentin.suhnjov@wisercat.eu>
2017-11-25 02:59:16 +0200

    RIHAKB-397. Convert LDAP entity attribute mappers to ODM

--- backend/src/main/java/ee/ria/riha/domain/LdapRepository.java
+++ backend/src/main/java/ee/ria/riha/domain/LdapRepository.java
@@ -1,16 +1,17 @@
 package ee.ria.riha.domain;
 
 import ee.ria.riha.conf.ApplicationProperties.LdapRepositoryProperties;
+import ee.ria.riha.domain.model.LdapGroup;
 import ee.ria.riha.domain.model.LdapUser;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.ldap.core.AttributesMapper;
-import org.springframework.ldap.core.ContextMapper;
-import org.springframework.ldap.core.DirContextAdapter;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.core.support.LdapContextSource;
+import org.springframework.ldap.filter.AbstractFilter;
 import org.springframework.ldap.filter.EqualsFilter;
 import org.springframework.ldap.filter.LikeFilter;
 import org.springframework.ldap.filter.OrFilter;
+import org.springframework.ldap.query.LdapQuery;
+import org.springframework.ldap.query.LdapQueryBuilder;
 import org.springframework.ldap.support.LdapNameBuilder;
 import org.springframework.ldap.support.LdapUtils;
 import org.springframework.util.Assert;
@@ -23,34 +24,11 @@ import java.util.Set;
 @Slf4j
 public class LdapRepository {
 
-    private static final String PERSONAL_CODE_ATTRIBUTE = "uid";
-    private static final String GIVEN_NAME_ATTRIBUTE = "givenName";
-    private static final String SURNAME_ATTRIBUTE = "sn";
+    private static final String USER_ID_ATTRIBUTE = "uid";
     private static final String COMMON_NAME_ATTRIBUTE = "cn";
-    private static final String EMAIL_ATTRIBUTE = "mail";
     private static final String MEMBER_OF_ATTRIBUTE = "memberOf";
-    private static final String APPROVERS_GROUPS_COMMON_NAME_ATTRIBUTE_VALUE = "*-hindaja";
-
-    private static final AttributesMapper<LdapUser> LDAP_USER_ATTRIBUTES_MAPPER = attributes -> LdapUser.builder()
-            .personalCode((String) attributes.get(PERSONAL_CODE_ATTRIBUTE).get())
-            .givenName(attributes.get(GIVEN_NAME_ATTRIBUTE) == null
-                    ? null
-                    : (String) attributes.get(GIVEN_NAME_ATTRIBUTE).get())
-            .surname(attributes.get(SURNAME_ATTRIBUTE) == null
-                    ? null
-                    : (String) attributes.get(SURNAME_ATTRIBUTE).get())
-            .commonName(attributes.get(COMMON_NAME_ATTRIBUTE) == null
-                    ? null
-                    : (String) attributes.get(COMMON_NAME_ATTRIBUTE).get())
-            .mail(attributes.get(EMAIL_ATTRIBUTE) == null
-                    ? null
-                    : (String) attributes.get(EMAIL_ATTRIBUTE).get())
-            .build();
-
-    private static final ContextMapper<Name> GROUP_DN_CONTEXT_MAPPER = ctx -> {
-        DirContextAdapter dirContextAdapter = (DirContextAdapter) ctx;
-        return dirContextAdapter.getDn();
-    };
+    private static final String APPROVER_GROUP_COMMON_NAME_PATTERN = "*-hindaja";
+    private static final String ALL_NON_OPERATIONAL_ATTRIBUTES = "*";
 
     private Name baseDn;
     private Name userSearchBase;
@@ -72,33 +50,53 @@ public class LdapRepository {
 
     public List<LdapUser> findLdapUsersByPersonalCodes(Set<String> personalCodes) {
         if (personalCodes.isEmpty()) {
-            log.info("Nothing to look for as personal codes were not provided");
+            log.debug("Will not search for users since list of personal codes is empty");
             return new ArrayList<>();
         }
 
         OrFilter filter = new OrFilter();
-        for (String personalCode : personalCodes) {
-            filter.or(new EqualsFilter(PERSONAL_CODE_ATTRIBUTE, personalCode));
-        }
+        personalCodes.stream()
+                .map(personalCode -> new EqualsFilter(USER_ID_ATTRIBUTE, personalCode))
+                .forEach(filter::or);
 
-        return ldapTemplate.search(userSearchBase, filter.encode(), LDAP_USER_ATTRIBUTES_MAPPER);
+        return findLdapUsers(filter);
+    }
+
+    private List<LdapUser> findLdapUsers(AbstractFilter filter) {
+        LdapQuery query = LdapQueryBuilder.query()
+                .base(userSearchBase)
+                .attributes(ALL_NON_OPERATIONAL_ATTRIBUTES, MEMBER_OF_ATTRIBUTE)
+                .filter(filter);
+
+        return ldapTemplate.find(query, LdapUser.class);
     }
 
     public List<LdapUser> getAllApprovers() {
-        List<Name> approversGroupsEntryDNs = getApproverGroupsDNs();
+        List<LdapGroup> approverGroups = findLdapGroups(
+                new LikeFilter(COMMON_NAME_ATTRIBUTE, APPROVER_GROUP_COMMON_NAME_PATTERN));
 
-        OrFilter filter = new OrFilter();
-        for (Name entryDN : approversGroupsEntryDNs) {
-            LdapName fullDn = LdapNameBuilder.newInstance(baseDn).add(entryDN).build();
-            filter.or(new EqualsFilter(MEMBER_OF_ATTRIBUTE, fullDn.toString()));
+        if (approverGroups.isEmpty()) {
+            log.debug("Would not search for approvers since no approver groups found");
+            return new ArrayList<>();
         }
 
-        return ldapTemplate.search(userSearchBase, filter.encode(), LDAP_USER_ATTRIBUTES_MAPPER);
+        OrFilter filter = new OrFilter();
+        approverGroups.stream()
+                .map(group -> LdapNameBuilder.newInstance(baseDn)
+                        .add(group.getDn())
+                        .build()
+                        .toString())
+                .map(groupDn -> new EqualsFilter(MEMBER_OF_ATTRIBUTE, groupDn))
+                .forEach(filter::or);
+
+        return findLdapUsers(filter);
     }
 
-    private List<Name> getApproverGroupsDNs() {
-        LikeFilter filter = new LikeFilter(COMMON_NAME_ATTRIBUTE, APPROVERS_GROUPS_COMMON_NAME_ATTRIBUTE_VALUE);
+    private List<LdapGroup> findLdapGroups(AbstractFilter filter) {
+        LdapQuery query = LdapQueryBuilder.query()
+                .base(groupSearchBase)
+                .filter(filter);
 
-        return ldapTemplate.search(groupSearchBase, filter.encode(), GROUP_DN_CONTEXT_MAPPER);
+        return ldapTemplate.find(query, LdapGroup.class);
     }
 }
