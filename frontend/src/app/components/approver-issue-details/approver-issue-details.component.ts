1659c67b3a02d8179b18fa716b9ed694539cde89
Mark Kimask <mark.kimask@wisercat.eu>
2017-12-13 10:27:15 +0200

    RIHAKB-460 only RIA approver can resolve with verdict

--- frontend/src/app/components/approver-issue-details/approver-issue-details.component.ts
+++ frontend/src/app/components/approver-issue-details/approver-issue-details.component.ts
@@ -85,12 +85,12 @@ export class ApproverIssueDetailsComponent implements OnInit {
   canResolveWithVerdict(){
     let ret = false;
     if (this.feedback.status == 'OPEN'){
-      let bHasApproverRole = this.environmentService.getUserMatrix().hasApproverRole;
       if (this.feedback.type == this.globals.issue_type.TAKE_INTO_USE_REQUEST
         || this.feedback.type == this.globals.issue_type.MODIFICATION_REQUEST
         || this.feedback.type == this.globals.issue_type.FINALIZATION_REQUEST
         || this.feedback.type == this.globals.issue_type.ESTABLISHMENT_REQUEST){
-        ret = !!bHasApproverRole;
+          let userMatrix = this.environmentService.getUserMatrix();
+          ret = userMatrix.hasApproverRole && userMatrix.isRiaMember;
       }
     }
     return ret;
