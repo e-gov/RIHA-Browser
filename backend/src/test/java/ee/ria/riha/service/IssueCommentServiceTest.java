687e8fd11829ef4c880f079f4e9f006c67367ee0
Valentin Suhnjov <valentin.suhnjov@wisercat.eu>
2018-01-19 12:05:05 +0200

    RIHAKB-519. Use issue status update and issue comment models

--- backend/src/test/java/ee/ria/riha/service/IssueCommentServiceTest.java
+++ backend/src/test/java/ee/ria/riha/service/IssueCommentServiceTest.java
@@ -4,6 +4,7 @@ import ee.ria.riha.authentication.RihaOrganizationAwareAuthenticationToken;
 import ee.ria.riha.rules.CleanAuthentication;
 import ee.ria.riha.storage.domain.CommentRepository;
 import ee.ria.riha.storage.domain.model.Comment;
+import ee.ria.riha.web.model.IssueCommentModel;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -20,9 +21,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.*;
 
 /**
  * @author Valentin Suhnjov
@@ -60,7 +59,9 @@ public class IssueCommentServiceTest {
 
     @Test
     public void populatesAuthorAndOrganizationFromUserDetailsDuringCommentCreation() {
-        issueCommentService.createIssueComment(EXISTING_ISSUE_ID, "");
+        issueCommentService.createIssueComment(EXISTING_ISSUE_ID, IssueCommentModel.builder()
+                .comment("")
+                .build());
 
         ArgumentCaptor<Comment> commentArgumentCaptor = ArgumentCaptor.forClass(Comment.class);
         verify(commentRepository).add(commentArgumentCaptor.capture());
@@ -76,6 +77,9 @@ public class IssueCommentServiceTest {
     public void throwsExceptionWhenActiveOrganizationIsNotSetDuringCommentCreation() {
         authenticationToken.setActiveOrganization(null);
 
-        issueCommentService.createIssueComment(EXISTING_ISSUE_ID, "comment");
+        issueCommentService.createIssueComment(EXISTING_ISSUE_ID, IssueCommentModel.builder()
+                .comment("comment")
+                .build());
     }
+
 }
