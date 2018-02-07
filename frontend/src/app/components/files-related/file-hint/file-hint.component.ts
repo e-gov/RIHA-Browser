cfd84d94e6e367cdabbb9e84acfe8271c78eb79f
Mark Kimask <mark.kimask@wisercat.eu>
2018-02-07 14:38:24 +0200

    RIHAKB-551 use correct icons for files

---
+++ frontend/src/app/components/files-related/file-hint/file-hint.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-file-hint',
+  templateUrl: './file-hint.component.html',
+  styleUrls: ['./file-hint.component.scss']
+})
+export class FileHintComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
