c6ee3cd22eb030b327e3e4fffa935cf1005ab984
Valentin  Suhnjov <valentin.suhnjov@wisercat.eu>
2018-02-08 19:08:09 +0200

    RIHAKB-526. Use system UUID instead of short name to provide path to system detail view

--- backend/src/main/java/ee/ria/riha/service/notification/model/InfoSystemDataModel.java
+++ backend/src/main/java/ee/ria/riha/service/notification/model/InfoSystemDataModel.java
@@ -2,6 +2,8 @@ package ee.ria.riha.service.notification.model;
 
 import lombok.*;
 
+import java.util.UUID;
+
 /**
  * Simplified info system data model that is used for communication with notification service.
  */
@@ -14,5 +16,6 @@ public class InfoSystemDataModel {
 
     private String fullName;
     private String shortName;
+    private UUID uuid;
 
 }
