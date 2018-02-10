7a2d180f6f5087e1504cbf988a78e87d2e55d02f
Valentin  Suhnjov <valentin.suhnjov@wisercat.eu>
2018-02-10 18:25:08 +0200

    RIHAKB-556. Alter logger message, improve tests

--- backend/src/test/java/ee/ria/riha/authentication/OrganizationRoleMappingExtractorTest.java
+++ backend/src/test/java/ee/ria/riha/authentication/OrganizationRoleMappingExtractorTest.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 import org.junit.runners.Parameterized.Parameter;
+import org.mockito.internal.matchers.Or;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -17,41 +19,43 @@ import static org.hamcrest.CoreMatchers.equalTo;
 @RunWith(Parameterized.class)
 public class OrganizationRoleMappingExtractorTest {
 
-    @Parameters(name = "{index}: extract({0}), expectedCode = {1} and expectedRole = {2}")
+    @Parameters(name = "{index}: extract({0}), expectedOrganizationRoleMapping = {1}")
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
-                {"12345-hindaja", "12345", "ROLE_HINDAJA"},
-                {"123-456-789-kirjeldaja", "123-456-789", "ROLE_KIRJELDAJA"},
-                {"12345-testija", "12345", "ROLE_TESTIJA"},
-                {null, null, null},
-                {"", null, null},
-                {"-testija", null, null},
-                {"123-456-abc_def-", null, null},
-                {"this-is-valid-1234567890-.&!+-#$%^-nA_Me", "this-is-valid-1234567890-.&!+-#$%^", "ROLE_NA_ME"},
-                {"this-is-not-valid-common-name!", null, null},
-                {"atLeastOneDashMustPresent", null, null}});
+                {"12345-hindaja", createExpectedOrganizationRoleMapping("12345", "ROLE_HINDAJA")},
+                {"123-456-789-kirjeldaja", createExpectedOrganizationRoleMapping("123-456-789", "ROLE_KIRJELDAJA")},
+                {"12345-testija", createExpectedOrganizationRoleMapping("12345", "ROLE_TESTIJA")},
+                {null, null},
+                {"", null},
+                {"-testija", null},
+                {"123-456-abc_def-", null},
+                {"this-is-valid-1234567890-.&!+-#$%^-nA_Me", createExpectedOrganizationRoleMapping(
+                        "this-is-valid-1234567890-.&!+-#$%^", "ROLE_NA_ME")},
+                {"this-is-not-valid-common-name!", null},
+                {"atLeastOneDashMustPresent", null}});
     }
 
-    private static final String DISPLAY_NAME_INPUT = "Display name is unnecessary in testing purposes";
+    private static final String DISPLAY_NAME = "Display name is unnecessary in testing purposes";
     private OrganizationRoleMappingExtractor organizationRoleMappingExtractor = new OrganizationRoleMappingExtractor();
-    private List<String> invalidCommonNames = Arrays.asList(null, "", "-testija", "123-456-abc_def-",
-            "this-is-not-valid-common-name!", "atLeastOneDashMustPresent");
 
     @Parameter
     public String commonNameInput;
     @Parameter(1)
-    public String expectedOrganizationCode;
-    @Parameter(2)
-    public String expectedOrganizationRole;
+    public OrganizationRoleMapping expectedOrganizationRoleMapping;
+
+    private static OrganizationRoleMapping createExpectedOrganizationRoleMapping(String expectedCode, String expectedRole) {
+        OrganizationRoleMapping expectedOrganizationRoleMapping = new OrganizationRoleMapping();
+
+        expectedOrganizationRoleMapping.setCode(expectedCode);
+        expectedOrganizationRoleMapping.setAuthority(new SimpleGrantedAuthority(expectedRole));
+        expectedOrganizationRoleMapping.setName(DISPLAY_NAME);
+
+        return expectedOrganizationRoleMapping;
+    }
 
     @Test
     public void test() {
-        OrganizationRoleMapping organizationRoleMapping = organizationRoleMappingExtractor.extract(commonNameInput, DISPLAY_NAME_INPUT);
-        if (organizationRoleMapping == null) {
-            assertThat(invalidCommonNames.contains(commonNameInput), is(true));
-        } else {
-            assertThat(organizationRoleMapping.getCode(), is(equalTo(expectedOrganizationCode)));
-            assertThat(organizationRoleMapping.getAuthority().getAuthority(), is(equalTo(expectedOrganizationRole)));
-        }
+        OrganizationRoleMapping organizationRoleMapping = organizationRoleMappingExtractor.extract(commonNameInput, DISPLAY_NAME);
+        assertThat(organizationRoleMapping, equalTo(expectedOrganizationRoleMapping));
     }
 }
