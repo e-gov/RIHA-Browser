ff4038102bc5bd00dc6c60a30b46cfa6cdb9bfc1
Valentin Suhnjov <valentin.suhnjov@wisercat.eu>
2018-03-08 16:43:43 +0200

    RIHAKB-580. Move organization system issues service to issue controller

--- backend/src/main/java/ee/ria/riha/web/DashboardController.java
+++ backend/src/main/java/ee/ria/riha/web/DashboardController.java
@@ -7,7 +7,9 @@ import ee.ria.riha.storage.util.PagedResponse;
 import ee.ria.riha.web.model.DashboardIssue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static ee.ria.riha.conf.ApplicationProperties.API_V1_PREFIX;
 
@@ -27,14 +29,6 @@ public class DashboardController {
     public ResponseEntity<PagedResponse<DashboardIssue>> listUserActiveOrganizationRelatedIssues(Pageable pageable,
                                                                                                  CompositeFilterRequest filterRequest) {
         return ResponseEntity.ok(dashboardService.listIssuesMentioningOrganization(filterRequest, pageable));
-    }
-
-    @GetMapping("/organizationIssues/{organizationCode}")
-    public ResponseEntity<PagedResponse<DashboardIssue>> listOrganizationInfoSystemsRelatedIssues(
-            @PathVariable("organizationCode") String organizationCode, Pageable pageable,
-            CompositeFilterRequest filterRequest) {
-
-        return ResponseEntity.ok(dashboardService.listOrganizationIssues(filterRequest, pageable, organizationCode));
     }
 
     @Autowired
