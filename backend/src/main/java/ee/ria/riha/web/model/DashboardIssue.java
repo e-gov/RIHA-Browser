78374d658e47068818ba16d8f7943dcc6efa5a89
Valentin Suhnjov <valentin.suhnjov@wisercat.eu>
2018-03-08 16:57:56 +0200

    RIHAKB-580. Add creation date to dashboard issue model

--- backend/src/main/java/ee/ria/riha/web/model/DashboardIssue.java
+++ backend/src/main/java/ee/ria/riha/web/model/DashboardIssue.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 /**
  * Special model of {@link ee.ria.riha.domain.model.Issue} that is used to provide issues to
  * approvers dashboard.
@@ -15,6 +17,7 @@ import lombok.Setter;
 public class DashboardIssue {
 
     private Long id;
+    private Date dateCreated;
     private IssueType type;
     private String title;
     private String infoSystemFullName;
