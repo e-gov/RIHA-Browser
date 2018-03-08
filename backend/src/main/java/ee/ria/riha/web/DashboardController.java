946a3bac6f40a711172b3c9a7623b3866024a74c
Valentin Suhnjov <valentin.suhnjov@wisercat.eu>
2018-03-08 16:44:43 +0200

    RIHAKB-580. Secure dashboard services

--- backend/src/main/java/ee/ria/riha/web/DashboardController.java
+++ backend/src/main/java/ee/ria/riha/web/DashboardController.java
@@ -7,6 +7,7 @@ import ee.ria.riha.storage.util.PagedResponse;
 import ee.ria.riha.web.model.DashboardIssue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,12 +21,14 @@ public class DashboardController {
     private DashboardService dashboardService;
 
     @GetMapping("/issues/my")
+    @PreAuthorize("hasRole('ROLE_RIHA_USER')")
     public ResponseEntity<PagedResponse<DashboardIssue>> listUserRelatedIssues(Pageable pageable,
                                                                                CompositeFilterRequest filterRequest) {
         return ResponseEntity.ok(dashboardService.listIssuesMentioningUser(filterRequest, pageable));
     }
 
     @GetMapping("/issues/org")
+    @PreAuthorize("hasRole('ROLE_RIHA_USER')")
     public ResponseEntity<PagedResponse<DashboardIssue>> listUserActiveOrganizationRelatedIssues(Pageable pageable,
                                                                                                  CompositeFilterRequest filterRequest) {
         return ResponseEntity.ok(dashboardService.listIssuesMentioningOrganization(filterRequest, pageable));
