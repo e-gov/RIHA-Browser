ee34e4cbe79f4b0a48f9bfae0a653b6aaf1625e9
Aleksandr Ivanov <aleksandr@wemakesoftware.eu>
2019-11-20 02:09:18 +0200

    RIHAKB-414 csrf support implemented

--- frontend/src/app/http-interceptors/index.ts
+++ frontend/src/app/http-interceptors/index.ts
@@ -1,8 +1,10 @@
 import {HTTP_INTERCEPTORS} from '@angular/common/http';
 
 import {SessionInterceptor} from './session-interceptor';
+import {HttpXsrfInterceptor} from './xcsrf-interceptor';
 
 /** Http interceptor providers in outside-in order */
 export const httpInterceptorProviders = [
+  {provide: HTTP_INTERCEPTORS, useClass: HttpXsrfInterceptor, multi: true},
   {provide: HTTP_INTERCEPTORS, useClass: SessionInterceptor, multi: true},
 ];
