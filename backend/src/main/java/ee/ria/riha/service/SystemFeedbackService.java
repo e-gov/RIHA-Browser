10ba2e83a7b6c7a2c0b351667803a4d0661cd9d7
Aleksandr Ivanov <aleksandr@wemakesoftware.eu>
2020-08-27 10:09:39 +0300

    certificate support

--- backend/src/main/java/ee/ria/riha/service/SystemFeedbackService.java
+++ backend/src/main/java/ee/ria/riha/service/SystemFeedbackService.java
@@ -1,6 +1,7 @@
 package ee.ria.riha.service;
 
 import com.fasterxml.jackson.databind.*;
+import ee.ria.riha.conf.*;
 import ee.ria.riha.domain.model.*;
 import lombok.extern.slf4j.*;
 import org.springframework.beans.factory.annotation.*;
@@ -9,18 +10,29 @@ import org.springframework.stereotype.*;
 import org.springframework.util.*;
 import org.springframework.web.client.*;
 
+import static ee.ria.riha.conf.ApplicationConfiguration.*;
+
 @Service
 @Slf4j
 public class SystemFeedbackService {
 
-    @Value("${browser.feedbackUrl}")
-    private String feedbackEndpointUrl;
+    private final FeedbackServiceConnectionProperties feedbackServiceConnectionProperties;
+
+    private final ObjectMapper objectMapper;
+
+    private final RestTemplate restTemplate;
 
     @Autowired
-    private ObjectMapper objectMapper;
+    public SystemFeedbackService(FeedbackServiceConnectionProperties feedbackServiceConnectionProperties,
+                                 ObjectMapper objectMapper,
+                                 @Qualifier(FEEDBACK_SERVICE) RestTemplate restTemplate) {
+        this.feedbackServiceConnectionProperties = feedbackServiceConnectionProperties;
+        this.objectMapper = objectMapper;
+        this.restTemplate = restTemplate;
+    }
 
     public void sendFeedback(SystemFeedbackModel systemFeedbackModel) {
-        RestTemplate restTemplate = new RestTemplate();
+
         try {
             MultiValueMap<String, String> headers = new HttpHeaders();
             headers.add("Content-Type", "application/json");
@@ -29,7 +41,7 @@ public class SystemFeedbackService {
             log.debug("prepared request for sending {}", requestEntity);
 
             ResponseEntity<String> exchange = restTemplate.exchange(
-                    feedbackEndpointUrl,
+                    feedbackServiceConnectionProperties.getUrl(),
                     HttpMethod.POST,
                     requestEntity,
                     String.class);
