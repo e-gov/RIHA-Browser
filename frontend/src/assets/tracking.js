826b427237204641414193e3a71c6cbb7621bb98
kristjankr <kristjan.kruus@ria.ee>
2020-09-10 14:50:59 +0300

    Revert matomo for now

---
+++ frontend/src/assets/tracking.js
@@ -0,0 +1,8 @@
+var idSite = 16;
+var matomoTrackingApiUrl = 'https://matomo.ria.ee/matomo.php';
+
+var _paq = window._paq || [];
+_paq.push(['setTrackerUrl', matomoTrackingApiUrl]);
+_paq.push(['setSiteId', idSite]);
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
