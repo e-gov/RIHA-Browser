adef4edf3765715daa05e4021f82171880ee53ac
Kristjan Kruus <kristjan.kruus@ria.ee>
2020-09-11 13:44:06 +0300

    added organization chooser and alias for getUserMatrix

--- frontend/src/app/components/approver-dashboard/approver-dashboard.component.ts
+++ frontend/src/app/components/approver-dashboard/approver-dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { GeneralHelperService } from '../../services/general-helper.service';
 import { EnvironmentService } from '../../services/environment.service';
+import {ModalHelperService} from "../../services/modal-helper.service";
+import {ActiveOrganizationChooserComponent} from '../active-organization-chooser/active-organization-chooser.component';
 
 @Component({
   selector: 'app-approver-dashboard',
@@ -9,9 +11,17 @@ import { EnvironmentService } from '../../services/environment.service';
 })
 export class ApproverDashboardComponent implements OnInit {
 
+  openOrganizationsModal() {
+    const modalRef = this.modalService.open(ActiveOrganizationChooserComponent);
+    return false;
+  }
+  
   constructor(public helper: GeneralHelperService,
               public generalHelperService: GeneralHelperService,
-              public environmentService: EnvironmentService) { }
+              public environmentService: EnvironmentService,
+              private modalService: ModalHelperService) {
+    this.userMatrix = this.environmentService.getUserMatrix();
+  }
 
   ngOnInit() {
     this.generalHelperService.setRihaPageTitle('Hinda');
