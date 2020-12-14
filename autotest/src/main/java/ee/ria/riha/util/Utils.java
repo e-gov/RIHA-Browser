4c304d37fe53489b7aea853add94d742679aaf11
Aleksandr Ivanov <aleksandr@wemakesoftware.eu>
2020-12-14 03:46:12 +0200

    fixed broken selenium tests

--- autotest/src/main/java/ee/ria/riha/util/Utils.java
+++ autotest/src/main/java/ee/ria/riha/util/Utils.java
@@ -1,11 +1,22 @@
 package ee.ria.riha.util;
 
-import java.io.File;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.io.*;
 
 public class Utils {
     public static final String RESOURCES_PATH = "/src/test/resources/";
 
     public static String getFileResourcePath(File file) {
         return file.getAbsoluteFile().getParent() + RESOURCES_PATH + file.getName();
+    }
+
+    public static void modifiedSelectByValue(Select select, String valueToSelect) {
+        WebElement optionToSelect = select.getOptions()
+                .stream()
+                .filter(webElement -> webElement.getAttribute("value")
+                .endsWith(valueToSelect)).findFirst().orElseThrow(IllegalStateException::new);
+        select.selectByValue(optionToSelect.getAttribute("value"));
     }
 }
