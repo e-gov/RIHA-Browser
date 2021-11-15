8574598efa11f087812ea47e8b1f6bb34529a5fc
makar <makar.shokarev@wisercat.net>
2021-11-15 11:30:54 +0200

    Get health check information from properties

--- backend/src/main/java/ee/ria/riha/health/ServiceHealthIndicator.java
+++ backend/src/main/java/ee/ria/riha/health/ServiceHealthIndicator.java
@@ -1,7 +1,9 @@
 package ee.ria.riha.health;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -9,14 +11,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Component
+@PropertySource("classpath:heartbeat.properties")
 public class ServiceHealthIndicator implements HealthIndicator {
+
+    @Value("${app.name}")
+    private String appName;
+    @Value("${app.version}")
+    private String appVersion;
+    @Value("${app.packaging.time}")
+    private String appPackagingTime;
+
     @Override
     public Health health() {
         Health.Builder status = Health.up();
         Map<String, Object> details = new HashMap<>();
-        details.put("appName", "test");
-        details.put("version", "test");
-        details.put("packagingTime", LocalDateTime.now());
+        details.put("appName", appName);
+        details.put("version", appVersion);
+        details.put("packagingTime", appPackagingTime);
         details.put("appStartTime", LocalDateTime.now());
         details.put("serverTime", LocalDateTime.now());
 
