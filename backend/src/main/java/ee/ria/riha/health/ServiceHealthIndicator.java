0a53c5999b58af18233b2ff6d4c83eaf7654de83
makar <makar.shokarev@wisercat.net>
2021-11-15 13:11:23 +0200

    Added app start time

--- backend/src/main/java/ee/ria/riha/health/ServiceHealthIndicator.java
+++ backend/src/main/java/ee/ria/riha/health/ServiceHealthIndicator.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.context.annotation.PropertySource;
+
 import org.springframework.stereotype.Component;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,6 +25,9 @@ public class ServiceHealthIndicator implements HealthIndicator {
     @Value("${app.packaging.time}")
     private String appPackagingTime;
 
+    RuntimeMXBean runtimeBeen = ManagementFactory.getRuntimeMXBean();
+    Date appStartTime = new Date(runtimeBeen.getStartTime());
+
     @Override
     public Health health() {
         Health.Builder status = Health.up();
@@ -28,7 +35,7 @@ public class ServiceHealthIndicator implements HealthIndicator {
         details.put("appName", appName);
         details.put("version", appVersion);
         details.put("packagingTime", appPackagingTime);
-        details.put("appStartTime", LocalDateTime.now());
+        details.put("appStartTime", appStartTime);
         details.put("serverTime", LocalDateTime.now());
 
         return status.withDetails(details).build();
