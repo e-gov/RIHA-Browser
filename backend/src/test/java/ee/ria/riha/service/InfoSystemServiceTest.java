51a1bb76471a1ffc471b8b6278284874939aaffb
Vadim Nesmashnov <vadim.nes@gmail.com>
2021-11-19 12:27:23 +0200

    RIHAKB-1021, fixed test runner, externalized project version in pom files

--- backend/src/test/java/ee/ria/riha/service/InfoSystemServiceTest.java
+++ backend/src/test/java/ee/ria/riha/service/InfoSystemServiceTest.java
@@ -12,7 +12,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.stubbing.Answer;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -20,7 +20,7 @@ import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
