b47b883ba9bcd24df970947e8a26aba04fce8923
makar <makar.shokarev@wisercat.net>
2021-11-30 12:15:08 +0200

    Added ip to audit log

--- backend/src/main/java/ee/ria/riha/logging/auditlog/AuditLogger.java
+++ backend/src/main/java/ee/ria/riha/logging/auditlog/AuditLogger.java
@@ -2,14 +2,13 @@ package ee.ria.riha.logging.auditlog;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import ee.ria.riha.authentication.RihaOrganization;
 import ee.ria.riha.authentication.UserContext;
-import ee.ria.riha.service.IllegalBrowserStateException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import static ee.ria.riha.service.SecurityContextUtil.getActiveOrganization;
+import javax.servlet.http.HttpServletRequest;
 
+import static ee.ria.riha.service.SecurityContextUtil.getActiveOrganization;
 
 @Slf4j
 @Component
@@ -26,7 +25,7 @@ public class AuditLogger {
      * Auditlog.
      * data objects will be serialized to JSON.
      */
-    public void log(AuditEvent auditEvent, AuditType auditType, Object... data) {
+    public void log(AuditEvent auditEvent, AuditType auditType, HttpServletRequest request, Object... data) {
         StringBuilder sb = new StringBuilder();
         var userIdO = userContext.getRihaUserId();
         String userId = userIdO.isEmpty() ? "0" : userIdO.get();
@@ -44,12 +43,23 @@ public class AuditLogger {
         }
 
         sb.append(String.format(" %s:%s; ", auditEvent, auditType));
-        sb.append(String.format("%s:%s:%s; ", userId, userName, organization));
+        sb.append(String.format("%s:%s:%s:%s; ", userId, userName, organization, getClientIpAddress(request)));
         try {
             sb.append(mapper.writeValueAsString(data));
         } catch (JsonProcessingException e) {
             log.warn("Unable to serialize audit data to JSON");
         }
         log.info(sb.toString());
+    }
+
+    private static String getClientIpAddress(HttpServletRequest request) {
+        String remoteAddress = "";
+        if (request != null){
+            remoteAddress = request.getHeader("X-FORWARDED-FOR");
+            if (remoteAddress == null || remoteAddress.equals("")){
+                remoteAddress = request.getRemoteAddr();
+            }
+        }
+        return remoteAddress;
     }
 }
