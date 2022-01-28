ef0262b26d90b2d06f16dd20b1bd533c88ecd549
makar <makar.shokarev@wisercat.net>
2022-01-28 10:33:50 +0200

    Added new logic to controller IssueController.getInfosystemIssue

--- backend/src/test/java/ee/ria/riha/service/auth/IssueAuthorizationServiceTest.java
+++ backend/src/test/java/ee/ria/riha/service/auth/IssueAuthorizationServiceTest.java
@@ -1,9 +1,9 @@
 package ee.ria.riha.service.auth;
 
-import ee.ria.riha.domain.model.InfoSystem;
-import ee.ria.riha.domain.model.Issue;
+import ee.ria.riha.domain.model.*;
 import ee.ria.riha.service.InfoSystemService;
 import ee.ria.riha.service.IssueService;
+import ee.ria.riha.web.IssueController;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
