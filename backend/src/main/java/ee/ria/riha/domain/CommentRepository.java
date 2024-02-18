1b3e4239a80f53ed299e8b4b29f7d54a153c5ee1
Martin A <martin.apalkov@wisercat.eu>
2024-02-18 14:37:30 +0200

    RIHAKB-1147 removed cache

--- backend/src/main/java/ee/ria/riha/domain/CommentRepository.java
+++ backend/src/main/java/ee/ria/riha/domain/CommentRepository.java
@@ -28,47 +28,39 @@ public class CommentRepository implements StorageRepository<Long, Comment> {
     }
 
     @Override
-    @Cacheable("listComment")
     public PagedResponse<Comment> list(Pageable pageable, Filterable filterable) {
         return storageClient.list(COMMENT_PATH, pageable, filterable, Comment.class);
     }
 
-    @Cacheable("listIssues")
     public PagedResponse<Comment> listIssues(Pageable pageable, Filterable filterable) {
         return storageClient.list(COMMENT_TYPE_ISSUE_VIEW_PATH, pageable, filterable, Comment.class);
     }
 
-    @Cacheable("listDashboardIssues")
     public PagedGridResponse<Comment> listDashboardIssues(Pageable pageable, CompositeFilterRequest filterRequest) {
         return storageClient.list(DASHBOARD_COMMENT_PATH, filterRequest, pageable);
     }
 
     @Override
-    @Cacheable("getComment")
     public Comment get(Long id) {
         return storageClient.get(COMMENT_PATH, id, Comment.class);
     }
 
     @Override
-    @Cacheable("findComment")
     public List<Comment> find(Filterable filterable) {
         return storageClient.find(COMMENT_PATH, filterable, Comment.class);
     }
 
     @Override
-    @CacheEvict(value={"listComment", "listIssues", "listDashboardIssues", "getComment", "findComment"}, allEntries = true)
     public List<Long> add(Comment entity) {
         return storageClient.create(COMMENT_PATH, entity);
     }
 
     @Override
-    @CacheEvict(value={"listComment", "listIssues", "listDashboardIssues", "getComment", "findComment"}, allEntries = true)
     public void update(Long id, Comment entity) {
         storageClient.update(COMMENT_PATH, id, entity);
     }
 
     @Override
-    @CacheEvict(value={"listComment", "listIssues", "listDashboardIssues", "getComment", "findComment"}, allEntries = true)
     public void remove(Long id) {
         storageClient.remove(COMMENT_PATH, id);
     }
