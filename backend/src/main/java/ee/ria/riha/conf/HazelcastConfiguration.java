2d33dee96d57fb691d2d0b490b881278adb9cd3a
Kristjan Kruus <kristjan.kruus@ria.ee>
2025-08-13 11:16:56 +0000

    fix hazelcast

--- backend/src/main/java/ee/ria/riha/conf/HazelcastConfiguration.java
+++ backend/src/main/java/ee/ria/riha/conf/HazelcastConfiguration.java
@@ -4,32 +4,21 @@ import com.hazelcast.config.Config;
 import com.hazelcast.config.TcpIpConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.web.WebFilter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
 
 import java.util.List;
-import java.util.Properties;
 
 @Configuration
+@EnableHazelcastHttpSession
 @Slf4j
 public class HazelcastConfiguration {
     @Value("${clustering.hazelcast.members:127.0.0.1}")
     private List<String> members;
-
-    @Bean
-    public WebFilter webFilter(@Qualifier("riha") HazelcastInstance hazelcastInstance) {
-
-        Properties properties = new Properties();
-        properties.put("instance-name", hazelcastInstance.getName());
-        properties.put("sticky-session", "false");
-
-        return new WebFilter(properties);
-    }
 
     @Qualifier("riha")
     @Bean
